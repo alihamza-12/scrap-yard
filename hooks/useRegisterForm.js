ccaf626158d324965d388d2b837b2e097302d4c1
alihamza-12 <alihamza-12@example.com>
2025-11-19 03:08:22 +0500

    useRegisterForm Custom hook created

---
+++ hooks/useRegisterForm.js
@@ -0,0 +1,78 @@
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useUserRegisterMutation } from "../store/userSlice";
+
+//Zod Schema
+const schema = z
+  .object({
+    fullName: z.string().min(3, { message: "Atleast 3 Words" }),
+    email: z.string().email({ message: "InValid Email Address" }),
+    address: z.string({ message: "Address Required" }),
+    phoneNo: z
+      .string()
+      .max(11, { message: "Enter Correct 11 Digits for Phone Number" }),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .max(32, "Password cannot exceed 32 characters")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/\d/, "Password must contain at least one number")
+      .regex(
+        /[!@#$%^&*(),.?":{}|<>]/,
+        "Password must contain at least one special character"
+      ),
+    confirmPass: z
+      .string()
+      .min(8, { message: "InValid Password Minimum 8 characters" }),
+    role: z.enum(["buyer", "seller"], { message: "Please Choice the Role" }),
+  })
+  .refine((data) => data.password === data.confirmPass, {
+    message: "Password donot Match",
+    path: ["confirmPass"],
+  });
+
+const useRegisterForm = () => {
+  const [active, setActive] = useState("");
+  //RTK query Mutation
+  const [userRegister, { error }] = useUserRegisterMutation();
+
+  //SetUp React Hook Form
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const setHandleRole = (role) => {
+    setActive(role);
+    setValue("role", role);
+  };
+  const onSubmit = (data) => {
+    // console.log(data);
+    userRegister({
+      Email: data.email,
+      Password: data.password,
+      FullName: data.fullName,
+      Address: data.address,
+      PhoneNo: data.phoneNo,
+      Role: data.role,
+    });
+  };
+  return {
+    register,
+    handleSubmit,
+    errors,
+    active,
+    setHandleRole,
+    onSubmit,
+    error,
+  };
+};
+
+export default useRegisterForm;
