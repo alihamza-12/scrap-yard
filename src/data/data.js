dbabd1db10d72f79c1582d6dae9420ef4bc9be95
alihamza-12 <alihamza-12@example.com>
2025-11-26 16:48:08 +0500

    Blogs Page & data is Showing of every Blog

---
+++ src/data/data.js
@@ -0,0 +1,173 @@
+import blog1 from "../assets/blogs/bg1.jpg";
+import blog2 from "../assets/blogs/bg2.jpg";
+import blog3 from "../assets/blogs/bg3.jpg";
+import blog4 from "../assets/blogs/bg4.jpg";
+import blog5 from "../assets/blogs/bg5.jpg";
+import blog6 from "../assets/blogs/bg6.png";
+
+export const blogData = [
+  {
+    id: 1,
+    imgSrc: blog1,
+    title: "The Environmental Impact of Throwing Away Scrap",
+    desc: "Explore the environmental consequences of discarding scrap items instead of recycling or selling them. Highlight the benefits of recycling and how it reduces landfill waste and pollution.",
+  },
+  {
+    id: 2,
+    imgSrc: blog2,
+    title: "How to Turn Your Scrap into Cash",
+    desc: 'Provide a detailed guide on how individuals can sell their scrap items on "Scrap Yard." Include tips on identifying valuable scrap, preparing it for sale, and setting a competitive price.',
+  },
+  {
+    id: 3,
+    imgSrc: blog3,
+    title: "Common Household Items You Didn’t Know You Could Sell as Scrap",
+    desc: "List and describe everyday household items that can be sold as scrap. Educate readers on the potential value of these items and encourage them to declutter their homes responsibly.",
+  },
+  {
+    id: 4,
+    imgSrc: blog4,
+    title: "The Future of Recycling: Innovations in the Scrap Industry",
+    desc: "Discuss recent innovations and trends in the recycling and scrap industry. Highlight new technologies and methods that are making recycling more efficient and environmentally friendly",
+  },
+  {
+    id: 5,
+    imgSrc: blog5,
+    title: "Success Stories: How People Have Benefited from Selling Scrap",
+    desc: "Share real-life success stories of individuals who have made significant profits by selling their scrap items. Include quotes and testimonials to inspire and motivate readers.",
+  },
+  {
+    id: 6,
+    imgSrc: blog6,
+    title: "The Importance of Recycling: How Every Small Effort Counts",
+    desc: "Emphasize the importance of recycling and how individual efforts contribute to a larger environmental impact. Encourage readers to adopt sustainable practices in their daily lives. Recycling is more than just a buzzword; it’s a critical practice for preserving our planet. Every small effort counts, and here’s why recycling should be an integral part of your daily routine.",
+  },
+];
+export const products = [
+  {
+    title: "Automobiles parts",
+    desc: "We accept used car parts such as engines, transmissions, radiators, batteries, and catalytic converters. These parts can be recycled or refurbished for reuse, providing a cost-effective alternative to new parts. Recycling automobile parts helps reduce the demand for new raw materials, conserves energy, and minimizes environmental impact. Catalytic converters are especially valuable due to the precious metals they contain, like platinum, palladium, and rhodium, which can be recovered and reused.",
+    price: "Rs.100 to Rs.500/KG",
+    imgSrc: "./products/auto.jpg",
+  },
+  {
+    title: "Batteries",
+    desc: "We accept used car batteries, industrial batteries, and rechargeable batteries from electronic devices. These batteries contain valuable materials like lead, lithium, nickel, and cadmium, which can be recovered and reused. Proper recycling of batteries prevents harmful chemicals from leaching into the environment and helps recover valuable resources. It's important to handle batteries with care to prevent leakage and contamination during collection and transport.",
+    price: "Rs.350 to Rs.360/KG",
+    imgSrc: "./products/bt.jpg",
+  },
+  {
+    title: "construction debris",
+    desc: "We accept materials like concrete, bricks, and drywall from construction and demolition projects. Recycling construction debris helps reduce the need for new raw materials, conserves energy, and minimizes waste. Recycled materials can be used in new construction projects, road building, or as aggregate in concrete production. It's important to ensure that the debris is clean and free from contaminants to facilitate efficient recycling.",
+    price: "Rs.180 to Rs.185/KG",
+    imgSrc: "./products/construction-waste.jpg",
+  },
+  {
+    title: "copper",
+    desc: "We accept old or unused copper pipes from plumbing systems. Copper is highly valued for its recyclability and worth, as it can be recycled repeatedly without losing quality. Recycling copper piping helps conserve natural resources, reduce energy consumption, and minimize environmental impact. Clean and sorted copper is preferred to ensure efficient recycling and high-quality recycled products.",
+    price: "Rs.2385 to Rs.2400/KG",
+    imgSrc: "./products/copper.jpg",
+  },
+  {
+    title: "Electronics",
+    desc: "This category includes old computers, laptops, smartphones, tablets, and other electronic devices. These items often contain valuable metals like gold, silver, copper, and platinum, as well as useful components like circuit boards and processors. Recycling electronics not only recovers precious metals but also prevents hazardous substances like lead and mercury from polluting the environment. Proper recycling and disposal of electronics help minimize electronic waste, which is one of the fastest-growing waste streams globally.",
+    price: "Rs.395 to Rs.415/KG",
+    imgSrc: "./products/electronic.jpg",
+  },
+  {
+    title: "furniture",
+    desc: "We accept metal and wooden furniture that can be disassembled and recycled or repurposed. Recycling furniture helps reduce waste, conserve resources, and provide materials for new products. Metal parts can be recycled into new metal products, while wooden parts can be repurposed or recycled into particleboard or mulch. Clean and sorted furniture is preferred to ensure efficient recycling.",
+    price: "Rs.1000 to Rs.7000/KG",
+    imgSrc: "./products/furniture.jpg",
+  },
+  {
+    title: "glass",
+    desc: "We accept glass bottles, jars, and other glass items that can be melted down and reused. Glass is 100% recyclable and can be recycled indefinitely without losing quality. Recycling glass helps conserve natural resources, reduce energy consumption, and decrease greenhouse gas emissions. Clean and sorted glass is preferred to ensure efficient recycling. Different colors of glass are typically processed separately to maintain the quality of recycled products.",
+    price: "Rs.142 to Rs.370/KG",
+    imgSrc: "./products/glass-scrap.jpg",
+  },
+  {
+    title: "Household appliances",
+    desc: "Items like refrigerators, washing machines, dryers, microwaves, and air conditioners fall under this category. These appliances contain valuable metals like steel, copper, and aluminum, as well as other recyclable materials. Recycling household appliances prevents them from ending up in landfills, where they can release harmful substances. Proper disposal and recycling of appliances help recover useful materials and ensure hazardous components are handled safely.",
+    price: "Rs.420 to Rs.845/KG",
+    imgSrc: "./products/house.jpg",
+  },
+  {
+    title: "Metal",
+    desc: "Scrap metal includes both ferrous metals like iron and steel, and non-ferrous metals such as aluminum, copper, brass, and stainless steel. These metals are commonly found in household items, construction materials, and industrial equipment. Ferrous metals are magnetic and often used in heavy machinery and construction due to their strength. Non-ferrous metals, on the other hand, are lighter and more resistant to corrosion, making them ideal for electrical wiring, plumbing, and automotive parts. Scrap metal recycling helps conserve natural resources and reduces the need for mining.",
+    price: "Rs.420 to Rs.445/KG",
+    imgSrc: "./products/metal.jpg",
+  },
+  {
+    title: "old shoes",
+    desc: "We accept all kinds of shoes for scrap, regardless of their condition. Whether they are worn-out, broken, mismatched, or simply out of style, we take them all. Old sneakers, boots, sandals, and even high heels are welcome. If you have shoes with missing laces, torn soles, or scuffed surfaces, don’t throw them away just sell us",
+    price: "Rs.260 to Rs.780/KG",
+    imgSrc: "./products/old-shoe.jpg",
+  },
+  {
+    title: "Paper and cardboard",
+    desc: "We accept newspapers, office paper, cardboard boxes, and other paper products for recycling. Recycling paper and cardboard helps conserve trees, water, and energy, and reduces greenhouse gas emissions. Clean and dry paper materials are essential for efficient recycling. The recycled paper can be used to produce new paper products, reducing the demand for virgin paper and minimizing environmental impact.",
+    price: "Rs.58 to Rs.65/KG",
+    imgSrc: "./products/paper.jpg",
+  },
+  {
+    title: "plastic",
+    desc: "Various types of recyclable plastics are accepted, including PET bottles, HDPE containers, PVC pipes, and more. Plastic scrap recycling helps reduce plastic waste in landfills and oceans, conserves petroleum resources, and reduces greenhouse gas emissions. Clean and sorted plastic materials are essential for effective recycling and to produce high-quality recycled products. Different types of plastics are processed separately, as they have distinct properties and applications.",
+    price: "Rs.70 to Rs.85/KG",
+    imgSrc: "./products/plastic.jpg",
+  },
+  {
+    title: "Plumbing fixtures",
+    desc: "This category includes old pipes, faucets, and other plumbing materials made of copper, brass, or steel. These metals are highly recyclable and can be melted down and reused in new plumbing fixtures or other products. Recycling plumbing fixtures not only conserves raw materials but also reduces the environmental impact of mining and metal production. Clean and sorted metal scrap is preferred to ensure efficient processing and high-quality recycled materials.",
+    price: "Rs.200 to Rs.320/KG",
+    imgSrc: "./products/plumbing.jpg",
+  },
+  {
+    title: "rubber",
+    desc: "We accept used tires, rubber mats, and other rubber products for recycling. Recycling rubber helps reduce the demand for new materials, conserves energy, and minimizes waste. Recycled rubber can be used to produce new rubber products, asphalt, or as a fuel source. It's important to ensure that the rubber is clean and free from contaminants to facilitate efficient recycling.",
+    price: "Rs.242 to Rs.340/KG",
+    imgSrc: "./products/rubber2.jpg",
+  },
+  {
+    title: "stainless steel",
+    desc: "We accept stainless steel items such as kitchen sinks, utensils, and industrial equipment. Stainless steel is durable and highly recyclable, making it a valuable material for various applications. Recycling stainless steel helps conserve natural resources, reduce energy consumption, and minimize waste. It's important to ensure that the stainless steel is clean and free from contaminants to facilitate efficient recycling.",
+    price: "Rs.240 to Rs.260/KG",
+    imgSrc: "./products/stainless2.jpg",
+  },
+  {
+    title: "textile",
+    desc: "We accept old clothing, fabrics, and other textile products that can be recycled or reused. Recycling textiles helps reduce the demand for new materials, conserves water and energy, and minimizes waste. Textiles can be repurposed into new clothing, rags, insulation, or other products. Clean and sorted textiles are essential for efficient recycling and to produce high-quality recycled products.",
+    price: "Rs.170 to Rs.190/KG",
+    imgSrc: "./products/textile.webp",
+  },
+  {
+    title: "Tin can",
+    desc: "We accept all types of tin cans, including soda cans, sauce cans, and food cans. Tin cans are primarily made of steel with a thin layer of tin to prevent rusting. They are widely used for packaging food and beverages due to their durability and ease of recycling. Recycling tin cans helps conserve energy and raw materials, as producing new metal from recycled cans requires less energy compared to extracting metal from ore. Clean, empty cans are preferred for recycling to ensure they can be processed efficiently.",
+    price: "Rs.35 to Rs.40/KG",
+    imgSrc: "./products/tin.jpg",
+  },
+  {
+    title: "wires and cables",
+    desc: "We accept copper and aluminum wires and cables, often salvaged from electrical and electronic devices. These materials are valuable due to their conductivity and are commonly used in electrical systems and telecommunications. Recycling wires and cables helps conserve valuable metals and reduces the need for mining. It's important that the wires are free from excessive insulation or other contaminants to facilitate efficient recycling.",
+    price: "Rs.420 to Rs.2400/KG",
+    imgSrc: "./products/wire.jpg",
+  },
+  {
+    title: "wood scrap",
+    desc: "This includes pallets, old furniture, and other wooden items that can be repurposed or recycled. Recycling wood helps conserve forests, reduce waste, and provide materials for new products. Wood scrap can be processed into mulch, particleboard, or fuel for energy production. It's important to ensure that the wood is free from contaminants like paint or chemicals to facilitate safe and efficient recycling.",
+    price: "Rs.800 to Rs.5000/KG",
+    imgSrc: "./products/wood2.jpg",
+  },
+  {
+    title: "Aluminium",
+    desc: "We accept aluminium scrap including beverage cans, window frames, car parts, and household items. Aluminium scrap should be clean and free from excessive dirt, plastics, or other non-aluminium contaminants. Aluminium is highly recyclable and in demand for its lightweight and corrosion-resistant properties, useful in manufacturing and construction.",
+    price: "Rs.1475 to Rs.1485/KG",
+    imgSrc: "./products/aluminum.webp",
+  },
+  {
+    title: "Brass",
+    desc: "We are interested in purchasing brass scrap, including items such as old plumbing fixtures, musical instruments, hardware, and decorative pieces. Please ensure the brass items are free of significant impurities or non-brass materials. Brass scrap is valued for its recyclability and durability, making it a valuable material for a variety of industrial and decorative applications.",
+    price: "Rs.1075 to Rs.1100/KG",
+    imgSrc: "./products/brass.jpeg",
+  },
+];
--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import PageLayOut from '../components/PagesLayout/PageLayOut'
+import React from "react";
+import PageLayOut from "../components/PagesLayout/PageLayOut";
+import { blogData } from "../data/data";
 
 const Blogs = () => {
+  // console.log(blogData);
+  const handleClick=(url)=>{
+console.log(url)
+  }
   return (
     <div>
-      <PageLayOut/>
-    </div>
-  )
-}
+      <PageLayOut />
 
-export default Blogs
+      {/* Blogs */}
+      <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800 py-12 px-4 sm:px-6 lg:px-8">
+        <h1>No of Blogs : {blogData.length}</h1>
+        {blogData.map((blog) => (
+          <div
+            key={blog.id}
+            className="p-4 bg-white dark:bg-gray-700 rounded shadow mb-4"
+          >
+            <div>
+              <img src={blog.imgSrc} alt="" />
+            </div>
+            <div>
+              <h2 className="text-xl font-semibold">{blog.title}</h2>
+              <p className="text-gray-600 dark:text-gray-300">{blog.desc}</p>
+              <button onClick={()=>handleClick(blog.id)}>read more</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
